362842bf53b2ba0da3d0a79cfa92e7d8c49aef98
surfineer <ria.scholz@gmail.com>
2023-08-26 08:02:55 +0200

    added other functions

--- Palindrome_Test/main.py
+++ Palindrome_Test/main.py
@@ -1,18 +1,31 @@
-def test(word):
+def list_iteration_test(word):
   letters = list(word)
   if letters == letters[::-1]:
     return True
+  else:
+     return False
 
-def palindrome(word):
+def deque_pop_test(word):
+    from collections import deque
+    palindrome = deque([])
+    test = word
+    for letter in test:
+        palindrome.append(letter)
+    for letter in palindrome:
+        if palindrome.popleft() == palindrome.pop():
+            return "This is a palindrome!"
+        else:
+            return "Not a palindrome!"
+
+def recursion_test(word):
   if len(word)<=1:
     return True
   if word[0] != word[-1]:
     return False
-  return palindrome(word[1:-1]) #recursion
+  return recursion_test(word[1:-1]) #recursion
 
-word = input("Enter Word / Sentence to test for palindrome:\n> ").lower().replace(" ","")
-print()
-if test(word) == True:
-  print("Yep! Palindrome")
-else:
-  print("No palindrome.")
+
+word = "racecar"
+print(list_iteration_test(word))
+print(deque_pop_test(word))
+print(recursion_test(word))
