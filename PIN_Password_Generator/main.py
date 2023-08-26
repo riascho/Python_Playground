1b440f0ba71983539d5ce9cff6410fbcbdfce490
surfineer <ria.scholz@gmail.com>
2023-08-26 07:53:14 +0200

    updates cleaner code

--- PIN_Password_Generator/main.py
+++ PIN_Password_Generator/main.py
@@ -1,27 +1,32 @@
-import random
+import random, string
 
-choice=int(input("""Do you want to create a PIN or password? 
-Enter '1' for PIN and '2' for password
-> """))
-
-def pin_generator(digits):
-  print("PIN Generator")
+def pin_generator(length):
   pin=""
-  for i in range (digits):
+  for i in range (length):
     pin += str(random.randint(0,9))
   return pin 
 
-def password_generator(length):
-  print("PASSWORD Generator")
-  list=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","#","_","-","*","&","?","!"]  
-  password=""
-  for i in range(length):
-    password += random.choice(list)
+def password_generator(length): 
+  password=[]
+  if length >= 4:
+    password.append(string.ascii_uppercase[random.randrange(26)])
+    password.append(string.digits[random.randrange(9)])
+    for i in range(length-2):
+      password.append(random.choice(list))
+  else:
+    return "Password needs to be at least 4 characters long!"  
+  random.shuffle(password)
+  password = "".join(password)
   return password
 
-if choice == 1:
-    digits=int(input("How many digits do you need? "))
-    print("PIN: ",pin_generator(digits))
-elif choice == 2:
-    length=int(input("How long does your password need to be? Number of characters: "))
-    print("PASSWORD: ",password_generator(length))
+list = []
+for letter in string.ascii_letters:
+  list.append(letter)
+for number in string.digits:
+  list.append(number)
+for symbol in string.punctuation:
+  list.append(symbol)
+
+length = 8
+with open("password.txt", "w") as file:
+  file.write(password_generator(length))
--- Palindrome_Test/main.py
+++ Palindrome_Test/main.py
@@ -1,26 +1,18 @@
-
 def test(word):
   letters = list(word)
   if letters == letters[::-1]:
     return True
-
-  
-word = input("Enter Word / Sentence to test for palindrome:\n> ").lower().replace(" ","")
-print()
-if test(word) == True:
-  print("Yep! Palindrome")
-else:
-  print("No palindrome.")
-
-  
-############
-
 
 def palindrome(word):
   if len(word)<=1:
     return True
   if word[0] != word[-1]:
     return False
-  return palindrome(word[1:-1])
+  return palindrome(word[1:-1]) #recursion
 
-print(palindrome("racecar"))
+word = input("Enter Word / Sentence to test for palindrome:\n> ").lower().replace(" ","")
+print()
+if test(word) == True:
+  print("Yep! Palindrome")
+else:
+  print("No palindrome.")
