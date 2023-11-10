b8ea75a2a040cc437a002f732a76122ad69fffca
surfineer <123465523+surfineer@users.noreply.github.com>
2023-11-10 12:09:44 +0100

    added list comprehension to process_time

--- Process_Time/main.py
+++ Process_Time/main.py
@@ -5,15 +5,19 @@ start_time = time.time()
 
 ages = [12, 38, 34, 26, 21, 19, 67, 41, 17]
 
-print("none:")
+print("\nSimple Loop:")
 for age in ages: 
   if age >= 21:
     print(age)
 print("--- %s seconds ---" % (time.time() - start_time))
 
-print("continue:")
+print("\nLoop with continue:")
 for age in ages: 
   if age < 21:
     continue
   print(age)
+print("--- %s seconds ---" % (time.time() - start_time))
+
+print("\nList Comperehension:")
+print([age for age in ages if age >= 21])
 print("--- %s seconds ---" % (time.time() - start_time))
