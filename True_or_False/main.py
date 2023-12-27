0e37ad6c8a3766ed8bde412a91944ca3e3743e57
surfineer <123465523+surfineer@users.noreply.github.com>
2023-12-27 21:50:48 +0800

    true or false evaluator

---
+++ True_or_False/main.py
@@ -0,0 +1,13 @@
+def true_or_false(value):
+    if value:
+        return True
+    return False
+
+
+def simplified(value):
+    return bool(value)
+
+val = []
+
+print(true_or_false(val))
+print(simplified(val))
